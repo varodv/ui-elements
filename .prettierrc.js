c88ef5f11c6a65c649690dc34e3989ed18e93361
varodv <varodv@gmail.com>
2023-06-18 01:53:26 +0200

    Simplify prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  tabWidth: 2,
-  printWidth: 100,
-  semi: true,
   singleQuote: true,
-  trailingComma: 'none',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always'
+  htmlWhitespaceSensitivity: 'ignore',
+  proseWrap: 'always',
+  vueIndentScriptAndStyle: true,
 };

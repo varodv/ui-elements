e2306165739e58ed100b25cd34abadd29e4e90a1
varodv <varodv@gmail.com>
2023-06-19 18:01:51 +0200

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+};
---
+++ packages/vue/src/index.ts
@@ -0,0 +1,1 @@
+alert('@ui-elements/vue');
